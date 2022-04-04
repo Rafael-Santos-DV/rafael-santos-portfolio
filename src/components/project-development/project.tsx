aa126349f6ff81d556916892a85573751866d31e
Rafael Santos <santosrafaell187@gmail.com>
2022-04-04 09:31:04 -0300

    add meta tags

--- src/components/project-development/project.tsx
+++ src/components/project-development/project.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BoxDevelopment } from './style';
 
 const ContainerDevelopment: React.FC = () => (
-  <BoxDevelopment>Project sendo desenvolvido</BoxDevelopment>
+  <BoxDevelopment>Projeto sendo desenvolvido</BoxDevelopment>
 );
 
 export default ContainerDevelopment;
